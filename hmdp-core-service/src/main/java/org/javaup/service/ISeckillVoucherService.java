03128d7ebb76c1f0c4295d564ca11025e823b50a
newcentury-2001 <570383051@qq.com>
2026-02-26 11:12:33 +0800

    c1

--- hmdp-core-service/src/main/java/org/javaup/service/ISeckillVoucherService.java
+++ hmdp-core-service/src/main/java/org/javaup/service/ISeckillVoucherService.java
@@ -5,7 +5,7 @@ import org.javaup.entity.SeckillVoucher;
 import org.javaup.model.SeckillVoucherFullModel;
 
 /**
- * @program: 黑马点评-plus升级版实战项目。添加 阿星不是程序员 微信，添加时备注 点评 来获取项目的完整资料
+ * @program: 黑马点评-plus升级版实战项目。
  * @description: 秒杀优惠券 接口
  * @author: 阿星不是程序员
  **/
